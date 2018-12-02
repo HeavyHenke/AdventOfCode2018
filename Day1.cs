76347ae853d7491496d816c89c1f6b2f042fac2e
HeavyHenke <henrik@hellenborg.com>
2018-12-02 21:46:32 +0100

    Day 1

---
+++ Day1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AoC2018
+{
+    class Day1
+    {
+        public string CalcA()
+        {
+            var data = File.ReadAllLines("Day1.data");
+            int freq = 0;
+            foreach (var d in data)
+            {
+                var delta = int.Parse(d);
+                freq += delta;
+            }
+
+            return freq.ToString();
+        }
+
+        public string CalcB()
+        {
+            var data = File.ReadAllLines("Day1.data");
+            int freq = 0;
+            var visited = new HashSet<int>{0};
+
+            while (true)
+            {
+                foreach (var d in data)
+                {
+                    var delta = int.Parse(d);
+                    freq += delta;
+
+                    if (visited.Add(freq) == false)
+                        return freq.ToString();
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AoC2018
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string result = new Day1().CalcB();
+
+            Clipboard.SetText(result);
+            Console.WriteLine(result);
+        }
+    }
+}
